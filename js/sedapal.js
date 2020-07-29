213eddc7b5f6845822185b429bff235840deb8a7
MarcoCp <cpmarcoo@gmail.com>
2020-07-29 10:52:55 -0500

    Menu Escritorio

--- js/sedapal.js
+++ js/sedapal.js
@@ -3,6 +3,16 @@ $(".menu-hamburguesa").click(function(){
 	$(".menu-fullpage").toggle("abierto");
 });
 
+$(".submenu").hide();
+// $("nav ul li").hover(function(){
+// 	$(this).parent().find(".submenu:first").toggle("inline-grid");
+// });
+$("nav ul li").hover(function () {
+    $(this).children(".submenu:first").slideDown().show();
+}, function () {
+    $(this).children(".submenu:first").slideUp();
+});
+
 $(".desplegable").click(function(){
 	$(this).parent().find(".submenu:first").toggle("abierto");
 	$(this).parent().find(".desplegable").toggleClass("abierto");
